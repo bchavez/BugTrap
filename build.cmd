b6fcafe5625cc301abd0f6dbc4401a6eea52ddf3
nitrocaster <paul-kv@yandex.ru>
2015-10-07 13:22:43 +0300

    Fix x86_64 build environment.

--- build.cmd
+++ build.cmd
@@ -5,9 +5,12 @@ REM Uncomment to forcibly set the build version.
 REM set FORCE_VERSION=0.0.4-alpha4
 
 IF NOT DEFINED DevEnvDir (
-	IF DEFINED vs120comntools ( 
-		CALL "%vs120comntools%\vsvars32.bat"
-	)
+    IF DEFINED vs120comntools ( 
+        set envsetup=call "%vs120comntools%\..\..\VC\vcvarsall.bat"
+    ) else (
+        echo "VS120COMNTOOLS is not defined, aborting."
+        exit /b 1
+    )
 )
 
 rem Install additional libraries (vc_mbcsmfc + boost.regex)
@@ -21,9 +24,11 @@ set "INCLUDE=C:\Libraries\boost;%INCLUDE%"
 set "LIB=C:\Libraries\boost\stage\lib;%LIB%"
 
 rem Additional arguments required for msbuild to use INCLUDE and LIB 
-set "MSB_ADD_ARGS=/p:useenv=true"
+set "MSB_ADD_ARGS=-p:useenv=true"
 
 rem 32-bit client
+%envsetup% x86
+
 echo ">>>>>>>>>>>>>>>>>>>>>>>> DEBUG 32"
 msbuild source\BugTrap.vs2013.sln -p:Configuration=Debug -p:Platform=Win32 %MSB_ADD_ARGS%
 if %errorlevel% neq 0 exit /b %errorlevel%
@@ -41,6 +46,8 @@ msbuild source\BugTrap.vs2013.sln -p:Configuration=".NET Release" -p:Platform=Wi
 if %errorlevel% neq 0 exit /b %errorlevel%
 
 rem 64-bit client
+%envsetup% x86_amd64
+
 echo ">>>>>>>>>>>>>>>>>>>>>>>> DEBUG 64"
 msbuild source\BugTrap.vs2013.sln -p:Configuration=Debug -p:Platform=x64 %MSB_ADD_ARGS%
 if %errorlevel% neq 0 exit /b %errorlevel%
@@ -58,6 +65,8 @@ msbuild source\BugTrap.vs2013.sln -p:Configuration=".NET Release" -p:Platform=x6
 if %errorlevel% neq 0 exit /b %errorlevel%
 
 rem Examples
+%envsetup% x86
+
 echo "Building BugTrap.Examples [Debug|Win32]"
 msbuild source\BugTrap.Examples.vs2013.sln -p:Configuration=Debug -p:Platform=Win32 %MSB_ADD_ARGS%
 if %errorlevel% neq 0 exit /b %errorlevel%
@@ -74,6 +83,8 @@ echo "Building BugTrap.Examples [Unicode Release|Win32]"
 msbuild source\BugTrap.Examples.vs2013.sln -p:Configuration="Unicode Release" -p:Platform=Win32 %MSB_ADD_ARGS%
 if %errorlevel% neq 0 exit /b %errorlevel%
 
+%envsetup% x86_amd64
+
 echo "Building BugTrap.Examples [Debug|x64]"
 msbuild source\BugTrap.Examples.vs2013.sln -p:Configuration=Debug -p:Platform=x64 %MSB_ADD_ARGS%
 if %errorlevel% neq 0 exit /b %errorlevel%
@@ -89,6 +100,8 @@ if %errorlevel% neq 0 exit /b %errorlevel%
 echo "Building BugTrap.Examples [Unicode Release|x64]"
 msbuild source\BugTrap.Examples.vs2013.sln -p:Configuration="Unicode Release" -p:Platform=x64 %MSB_ADD_ARGS%
 if %errorlevel% neq 0 exit /b %errorlevel%
+
+%envsetup% x86
 
 echo "Building BugTrap.Examples [Debug|.NET x86]"
 msbuild source\BugTrap.Examples.vs2013.sln -p:Configuration=Debug -p:Platform=".NET x86" %MSB_ADD_ARGS%
